7139e9500eec6ba346a0914a02756808ab8bd1af
hpknollenberg <hpknollenberg@gmail.com>
2024-06-05 10:40:08 -0400

    likes working

--- src/UploadMessage.jsx
+++ src/UploadMessage.jsx
@@ -6,30 +6,42 @@ import { useContext, useState } from "react"
 const UploadImage = () => {
     const { auth } = useContext(AuthContext)
     const {profile, setProfile} = useContext(ProfileContext)
-    const [message, setMessage] = useState(undefined)
+    const [message, setMessage] = useState("")
+    const [image, setImage] = useState("")
 
     const submit = () => {
         createMessage({ 
             auth,
             message,
+            image,
             profile,
         })
-            .then(response => {
-                console.log('UPLOAD MESSAGE: RESPONSE: ', response)
+            .then((response) => {
+                console.log("MESSAGE: ", response)
             })
             .catch(error => console.log('ERROR: ', error))
+        
+        setMessage("")
+        setImage("")
     }
 
     return (
         <div className='d-flex justify-content-start'>
             <div>                
-                <div>
+                <div style={{ margin: "5px" }}>
                     <input 
                         value = { message }
                         onChange={e => setMessage(e.target.value)}
                     />
                 </div>
-                <div>
+                <div style={{ margin: "5px" }}>
+                    <input
+                        accept='image/*'
+                        type='file'
+                        onChange={e => setImage(e.target.files[0])}
+                    />
+                </div>
+                <div style={{ margin: "5px" }}>
                     <button onClick={() => {submit()}}>
                         Send Message</button>
                 </div>
--- src/api.js
+++ src/api.js
@@ -2,36 +2,24 @@ import axios from 'axios'
 
 const baseUrl = "http://127.0.0.1:8000"
 
-export const getToken = ({ auth, username, password }) => {
-  axios.post(`${baseUrl}/token/`, {
-    username: username,
-    password: password
-  }).then(response => {
-    console.log('RESPONSE: ', response)
-    auth.setAccessToken(response.data.access)
-  })
-  .catch(error => {
-    console.log('ERROR: ', error)
-    auth.setAccessToken(undefined)
+
+
+export const createMessage = ({ auth, message, image, profile }) => {
+  return axios({
+    method: 'post',
+    url: `${baseUrl}/create-message/`,
+    headers: {
+      Authorization: `Bearer ${auth.accessToken}`,
+      'Content-Type': 'multipart/form-data'
+    },
+    data: {
+      message,
+      image,
+      profile
+    }
   })
 }
 
-export const fetchUser = ({ auth }) => {
-  return axios({
-    method: 'get',
-    url: `${baseUrl}/profile/`, 
-    headers: {
-      Authorization: `Bearer ${auth.accessToken}`
-    }
-  }).then(response => {
-    console.log('PROFILE: ', response)
-    return response
-  })
-  .catch(error => {
-    console.log('ERROR: ', error)
-    auth.setAccessToken(undefined)
-  })
-}
 
 export const createUser = ({ username, password, firstName, lastName }) => {
   axios({
@@ -52,58 +40,6 @@ export const createUser = ({ username, password, firstName, lastName }) => {
 }
 
 
-export const getImages = ({ auth }) => {
-  return axios({
-    method: 'get',
-    url: `${baseUrl}/get-images`,
-    headers: {
-      Authorization: `Bearer ${auth.accessToken}`
-    }
-  })
-}
-
-export const getMessages = ({ auth }) => {
-  return axios({
-    method: 'get',
-    url: `${baseUrl}/get-messages`,
-    headers: {
-      Authorization: `Bearer ${auth.accessToken}`
-    }
-  })
-}
-
-
-export const createImage = ({ title, image, auth }) => {
-  return axios({
-    method: 'post',
-    url: `${baseUrl}/create-image/`,
-    headers: {
-      Authorization: `Bearer ${auth.accessToken}`,
-      'Content-Type': 'multipart/form-data'
-    },
-    data: {
-      image,
-      title,
-    }
-  })
-}
-
-
-export const createMessage = ({ auth, message, profile }) => {
-  return axios({
-    method: 'post',
-    url: `${baseUrl}/create-message/`,
-    headers: {
-      Authorization: `Bearer ${auth.accessToken}`
-    },
-    data: {
-      message,
-      profile
-    }
-  })
-}
-
-
 export const deleteMessage = ({ auth, id }) => {
   return axios({
     method: 'delete',
@@ -116,3 +52,72 @@ export const deleteMessage = ({ auth, id }) => {
     }
   })
 }
+
+
+export const fetchUser = ({ auth }) => {
+  return axios({
+    method: 'get',
+    url: `${baseUrl}/profile/`, 
+    headers: {
+      Authorization: `Bearer ${auth.accessToken}`
+    }
+  }).then(response => {
+    console.log('PROFILE: ', response)
+    return response
+  })
+  .catch(error => {
+    console.log('ERROR: ', error)
+    auth.setAccessToken(undefined)
+  })
+}
+
+
+export const getMessages = ({ auth }) => {
+  return axios({
+    method: 'get',
+    url: `${baseUrl}/get-messages`,
+    headers: {
+      Authorization: `Bearer ${auth.accessToken}`
+    }
+  })
+}
+
+
+export const getToken = ({ auth, username, password }) => {
+  axios.post(`${baseUrl}/token/`, {
+    username: username,
+    password: password
+  }).then(response => {
+    console.log('RESPONSE: ', response)
+    auth.setAccessToken(response.data.access)
+  })
+  .catch(error => {
+    console.log('ERROR: ', error)
+    auth.setAccessToken(undefined)
+  })
+}
+
+
+export const updateLikes = ({ auth, id }) => {
+  return axios({
+    method: 'put',
+    url: `${baseUrl}/update-likes/`,
+    headers: {
+      Authorization: `Bearer ${auth.accessToken}`
+    },
+    data: {
+      id
+    }
+  }) 
+    .then(response => {
+      console.log("UPDATE LIKES: ", response)
+      return response
+    })
+    .catch(error => {
+      console.log("LIKES ERROR: ", error)
+    })
+}
+
+
+
+
